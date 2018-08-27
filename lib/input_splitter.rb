dc1d24694f1a2f7c1321a55a9069b62857fbc238
Simon Weiler <sweiler@thoughtworks.com>
2018-08-27 20:29:08 +0200

    add string splitting for simple case

--- lib/input_splitter.rb
+++ lib/input_splitter.rb
@@ -1,8 +1,20 @@
 class InputSplitter
-  def split64 block64
+  def split64(block64)
     [block64 >> (64 - 16),
      (block64 >> 32) & 0xffff,
      (block64 >> 16) & 0xffff,
      block64 & 0xffff]
+  end
+
+  def split_string(str)
+    blocks = []
+    for i in 0.step(str.bytesize - 1, 8)
+      block = 0
+      for j in 0..7
+        block |= str.getbyte(i + j) << (56 - (j * 8))
+      end
+      blocks.push(block)
+    end
+    blocks
   end
 end
--- spec/input_splitter_spec.rb
+++ spec/input_splitter_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe InputSplitter do
     blocks = input_splitter.split64(0x0123456789abcdef)
     expect(blocks).to eq([0x0123, 0x4567, 0x89ab, 0xcdef])
   end
+
+  it 'splits a string into 64bit blocks' do
+    input_splitter = InputSplitter.new
+    blocks = input_splitter.split_string('some str')
+
+    blocks_as_hex = blocks.each.map {|b| b.to_s(16)}
+    expect(blocks_as_hex[0]).to eq(chars_as_block('s', 'o', 'm', 'e', ' ', 's', 't', 'r'))
+  end
+end
+
+def chars_as_block(*chars)
+  shift = 56
+  res = 0
+  for i in 0..7
+    res = res | (chars[i].ord << shift)
+    shift -= 8
+  end
+  res.to_s(16)
 end
