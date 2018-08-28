699402977903ca28db90101fdda4cc918867cff9
Simon Weiler <sweiler@thoughtworks.com>
2018-08-28 20:40:38 +0200

    let key scheduler rotate the key

--- lib/key_scheduler.rb
+++ lib/key_scheduler.rb
@@ -1,9 +1,35 @@
 class KeyScheduler
-  def initialize(main_key)
+  def initialize(main_key, offset)
     @main_key = main_key
+    @offset = offset
+  end
+
+  def self.for_key(main_key)
+    new(main_key, 0)
   end
 
   def key(index)
-    (@main_key >> (128 - (index + 1) * 16)) & 0xffff
+    shift_by = (index + @offset) % 8
+
+    (key_for_index(index) >> (128 - (shift_by + 1) * 16)) & 0xffff
+  end
+
+  def next
+    KeyScheduler.new(@main_key, (@offset + 6) % 8)
+  end
+
+private
+  def rotated_key
+    overflow = @main_key >> (128 - 25)
+    key_with_25_zeros = (@main_key << 25) & 0xffffffffffffffffffffffffffffffff
+    key_with_25_zeros | overflow
+  end
+
+  def key_for_index(index)
+    if (index + @offset > 7)
+      return rotated_key
+    else
+      return @main_key
+    end
   end
 end
--- spec/key_scheduler_spec.rb
+++ spec/key_scheduler_spec.rb
@@ -3,13 +3,26 @@ require 'key_scheduler'
 RSpec.describe KeyScheduler do
   describe '#key' do
     it 'retrieves the first six keys' do
-      scheduler = KeyScheduler.new(0xFFFF0000EEEE1111DDDD2222CCCC3333)
+      scheduler = KeyScheduler.for_key(0xFFFF0000EEEE1111DDDD2222CCCC3333)
       expect(scheduler.key(0)).to eq(0xFFFF)
       expect(scheduler.key(1)).to eq(0x0000)
       expect(scheduler.key(2)).to eq(0xEEEE)
       expect(scheduler.key(3)).to eq(0x1111)
       expect(scheduler.key(4)).to eq(0xDDDD)
       expect(scheduler.key(5)).to eq(0x2222)
+    end
+  end
+
+  describe '#next' do
+    it 'creates the KeyScheduler for the next round' do
+      scheduler = KeyScheduler.for_key(0xFFFF0000EEEE1111DDDD2222CCCC3333)
+
+      next_round = scheduler.next
+
+      expect(next_round.key(0)).to eq(0xCCCC)
+      expect(next_round.key(1)).to eq(0x3333)
+      expect(next_round.key(2)).to eq(0x01dd)
+      expect(next_round.key(3)).to eq(0xdc22)
     end
   end
 end
