bbba09e0f0f3c1501dfe6f0d359636dd8344ee5c
Simon Weiler <sweiler@thoughtworks.com>
2018-08-29 17:01:51 +0200

    add more complex example for rounds

---
+++ lib/swap_round.rb
@@ -0,0 +1,5 @@
+class SwapRound
+  def run(blocks)
+    [blocks[0], blocks[2], blocks[1], blocks[3]]
+  end
+end
--- spec/single_round_spec.rb
+++ spec/single_round_spec.rb
@@ -1,9 +1,10 @@
 require 'inner_round'
 require 'half_round'
+require 'swap_round'
 require 'key_scheduler'
 require 'operations'
 
-RSpec.describe 'single round' do
+RSpec.describe 'rounds' do
   it 'encrypts and decrypts correctly in regular round' do
     input_blocks = [0x0123, 0x4567, 0x89ab, 0xcdef]
     key_scheduler = KeyScheduler.for_key(0xabab00edefEE111234D08152CCCC3333)
@@ -12,22 +13,56 @@ RSpec.describe 'single round' do
     dec_keys1 = key_scheduler.decrypt_keys(5)
     dec_keys2 = key_scheduler.decrypt_keys(6)
 
-    half_round1_enc = HalfRound.new(enc_keys1)
-    inner_round_enc = InnerRound.new(enc_keys1)
-    half_round2_enc = HalfRound.new(enc_keys2)
+    enc_rounds = [
+      HalfRound.new(enc_keys1),
+      InnerRound.new(enc_keys1),
+      HalfRound.new(enc_keys2)
+    ]
 
-    half_round1_dec = HalfRound.new(dec_keys1)
-    inner_round_dec = InnerRound.new(enc_keys1)
-    half_round2_dec = HalfRound.new(dec_keys2)
+    dec_rounds = [
+      HalfRound.new(dec_keys1),
+      InnerRound.new(enc_keys1),
+      HalfRound.new(dec_keys2)
+    ]
 
-    encrypted_blocks1 = half_round1_enc.run(input_blocks)
-    encrypted_blocks2 = inner_round_enc.run(encrypted_blocks1)
-    encrypted_blocks3 = half_round2_enc.run(encrypted_blocks2)
+    encrypted_blocks = enc_rounds.reduce(input_blocks) {|blocks, round| round.run(blocks)}
+    decrypted_blocks = dec_rounds.reduce(encrypted_blocks) {|blocks, round| round.run(blocks)}
 
-    decrypted_blocks1 = half_round1_dec.run(encrypted_blocks3)
-    decrypted_blocks2 = inner_round_dec.run(decrypted_blocks1)
-    decrypted_blocks3 = half_round2_dec.run(decrypted_blocks2)
+    expect(decrypted_blocks).to eq(input_blocks)
+  end
 
-    expect(decrypted_blocks3).to eq(input_blocks)
+  it 'encrypts and decrypts correctly with two rounds' do
+    input_blocks = [0x0123, 0x4567, 0x89ab, 0xcdef]
+    key_scheduler = KeyScheduler.for_key(0xabab00edefEE111234D08152CCCC3333)
+    enc_keys1 = key_scheduler.encrypt_keys(2)
+    enc_keys2 = key_scheduler.encrypt_keys(3)
+    enc_keys3 = key_scheduler.encrypt_keys(4)
+    dec_keys1 = key_scheduler.decrypt_keys(4)
+    dec_keys2 = key_scheduler.decrypt_keys(5)
+    dec_keys3 = key_scheduler.decrypt_keys(6)
+
+    swap = SwapRound.new
+    enc_rounds = [
+      HalfRound.new(enc_keys1),
+      InnerRound.new(enc_keys1),
+      swap,
+      HalfRound.new(enc_keys2),
+      InnerRound.new(enc_keys2),
+      HalfRound.new(enc_keys3)
+    ]
+
+    dec_rounds = [
+      HalfRound.new(dec_keys1),
+      InnerRound.new(enc_keys2),
+      swap,
+      HalfRound.new(swap.run(dec_keys2)),
+      InnerRound.new(enc_keys1),
+      HalfRound.new(dec_keys3)
+    ]
+
+    encrypted_blocks = enc_rounds.reduce(input_blocks) {|blocks, round| round.run(blocks)}
+    decrypted_blocks = dec_rounds.reduce(encrypted_blocks) {|blocks, round| round.run(blocks)}
+
+    expect(decrypted_blocks).to eq(input_blocks)
   end
 end
